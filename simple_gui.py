9a3d23c31203b2f8c2024906b94c6787aff02e95
Ryan Taylor <saltlakeryan@gmail.com>
2015-05-07 12:13:17 -0600

    need to fix 'return False' error

--- simple_gui.py
+++ simple_gui.py
@@ -21,42 +21,25 @@ class RunProgram (object):
         config = ConfigParser.ConfigParser()
         config.readfp(open(r'config.ini'))
         path = config.get('Section', 'front_window')
-        #os.startfile(path)
         p = subprocess.Popen(path)
         self.pid = p.pid
         print "self.pid"
         print self.pid
         print
     
-    def find_window_for_pid(self, pid):
-        global result
-        result = None
+    def makeProgramInFront(self, pid):
         def callback(hwnd, _):
-            global result
             ctid, cpid = win32process.GetWindowThreadProcessId(hwnd)
             print "cpid"
             print cpid
             if cpid == pid:
-                result = hwnd
                 win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,0,0,500,500,0)
                 print "hwnd"
                 print hwnd
-                print "result"
-                print result
                 return False
             return True
         win32gui.EnumWindows(callback, None)
-        print "final result"
-        print result
-        print
-        return result    
-    
-    def show(self):  
-        hwnd = self.find_window_for_pid(self.pid)  
-        #win32gui.SetForegroundWindow (hwnd)  
-        #win32gui.SetWindowPos(hwnd,win32con.HWND_TOPMOST,0,0,500,500,0)
-        
-     
+
            
 #end RunProgram class
 #=======================================================
@@ -68,14 +51,11 @@ if __name__ == '__main__' :
     frm.Show()
     runP = RunProgram()
     time.sleep (2)
-    runP.show()
+    runP.makeProgramInFront(runP.pid)
     windowList = []
     win32gui.EnumWindows(lambda hwnd, windowList: windowList.append((win32gui.GetWindowText(hwnd),hwnd)), windowList)
     print len(windowList)
     for i in windowList:
         print i
-    #cmdWindow = [i for i in windowList if 'New Tab - Google Chrome' in i[0].lower()]
-    #print len(cmdWindow)
-    #win32gui.SetWindowPos(cmdWindow[0][1],win32con.HWND_TOPMOST,0,0,100,100,0)
     app.MainLoop()
     
