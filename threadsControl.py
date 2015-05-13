f0b0d508e82d1e72bb565b30e2300ea873d67f4e
Ryan Taylor <saltlakeryan@gmail.com>
2015-05-13 14:45:42 -0600

    Do modificaion on frame, add password input, and make encrypt.

--- threadsControl.py
+++ threadsControl.py
@@ -1,5 +1,8 @@
-import wx, ConfigParser, os, win32gui, win32process, win32con, subprocess, time
+import wx, ConfigParser, win32gui, win32process, win32con, subprocess, time
 from threading import *
+import base64 
+
+ID_SUBMIT = wx.NewId()
 
 class OverlayFrame( wx.Frame )  :
  
@@ -7,11 +10,36 @@ class OverlayFrame( wx.Frame )  :
  
         wx.Frame.__init__( self, None, title="Transparent Window",
                            style=wx.DEFAULT_FRAME_STYLE | wx.STAY_ON_TOP )
- 
         self.ShowFullScreen( True )
-        self.alphaValue = 255
-        self.SetTransparent( self.alphaValue )
-                
+        self.SetBackgroundColour('#CCE8CF')
+        
+        font=wx.Font(16,wx.DECORATIVE,wx.NORMAL,wx.BOLD)
+        self.label = wx.StaticText(self, label="For Administrator Only:", pos=(10,10))
+        self.label.SetFont(font)
+        
+        self.inputField = wx.TextCtrl(self, value="", size=(140, 30), pos=(10,45), name="input",  style=wx.TE_PASSWORD)
+        self.inputField.SetFont(font)
+        
+        self.submitbutton = wx.Button(self, ID_SUBMIT, 'Submit', pos=(160,50))
+        self.submitbutton.SetFont(font)
+        self.Bind(wx.EVT_BUTTON, self.OnSubmit, id=ID_SUBMIT)
+        self.Bind(wx.EVT_TEXT_ENTER, self.OnSubmit)
+        self.input = None
+        
+    def OnSubmit(self, event):
+        self.input = self.inputField.GetValue()
+        encrypt = base64.encodestring(self.input) 
+        print encrypt
+        fp = open(r'config.ini')
+        config = ConfigParser.ConfigParser()
+        config.readfp(fp)
+        fp.close()
+        pw = config.get('Section', 'front_window')
+        # if encrypt == pw,destroy window, may do more on encrypt 
+        # else display error message
+        # close thread?
+        # input: do limitation
+        
     def OnCloseWindow( self, evt ) :
         self.Destroy()
     
@@ -37,8 +65,10 @@ def makeProgramAtFront():
         pass
                      
 def openCoral ():
+    fp = open(r'config.ini')
     config = ConfigParser.ConfigParser()
-    config.readfp(open(r'config.ini'))
+    config.readfp(fp)
+    fp.close()
     path = config.get('Section', 'front_window')
     subprocess.Popen(path)
     time.sleep (5)
@@ -64,7 +94,6 @@ if __name__ == '__main__' :
     app = wx.App( False )
     frm = OverlayFrame()
     frm.Show()
-
     thread = ControlFrameThread()
 
     app.MainLoop()
