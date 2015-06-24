510529b58b7e1e1220a86851790502e2505a4b73
chonghux <chonghux@eng.utah.edu>
2015-06-24 16:29:46 -0600

    make code concise

--- source/buildtools.py
+++
@@ -1,9 +0,0 @@
-import zipfile, os
-
-def zipdir(path, zipname):
-    zipf = zipfile.ZipFile(zipname,'w')
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            zipf.write(os.path.join(root, file))
-    zipf.close()
-
