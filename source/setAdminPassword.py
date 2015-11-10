128f9cf9cc9fc966fb67337c795b69bda788bf5a
chonghux <chonghux@eng.utah.edu>
2015-11-10 10:10:18 -0700

    delete comment

--- source/setAdminPassword.py
+++ source/setAdminPassword.py
@@ -1,14 +1,3 @@
-# import os,sys, wx, win32gui, win32con, time, thread, win32process, subprocess, ConfigParser, signal, pythoncom, pyHook, psutil, zope.interface, urllib2, cffi, cryptography
-# from twisted.internet import protocol, reactor, endpoints
-# from win32api import GetSystemMetrics
-# from threading import *
-# import screenlockConfig, screenlockController, version
-# from flask import Flask, request, Response
-# from functools import wraps
-# from urlparse import urlparse
-# from OpenSSL import SSL
-# import json
-
 import os, wx, screenlockConfig
 
 ID_SUBMIT = wx.NewId()
